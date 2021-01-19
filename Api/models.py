86e41db8291cd5c88ca68941717764b6c74f15ea
zhouguanglu2020 <zhouguanglu2020@163.com>
2021-01-19 17:23:34 +0800

    2021年1月19日，走了2

--- Api/models.py
+++ Api/models.py
@@ -16,7 +16,7 @@ class Users(models.Model):
     hour = models.IntegerField(default=0, verbose_name="学习时长")
     exp = models.IntegerField(default=0, verbose_name="学习经验数")
     integral = models.IntegerField(default=0, verbose_name="积分数")
-    follow = models.IntegerField(default=0, verbose_name="follow数")
+    follow = models.IntegerField(default=0, verbose_name="关注数")
     fans = models.IntegerField(default=0, verbose_name="粉丝数")
     email = models.CharField(max_length=255, default="", verbose_name="邮箱")
     qq = models.CharField(max_length=255, default="", verbose_name="qq")
@@ -24,3 +24,37 @@ class Users(models.Model):
     wechat = models.CharField(max_length=255, default="", verbose_name="微信")
     weibo = models.CharField(max_length=255, default="", verbose_name="微博")
     create_time = models.DateTimeField(auto_now=True, verbose_name="注册时间")
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+class Bill(models.Model):
+    '''
+    订单信息
+    '''
+    userid = models.CharField(max_length=255, verbose_name="用户id")
+    orderno = models.CharField(max_length=255, verbose_name="订单号")
+    name = models.CharField(max_length=255, verbose_name="课程名字")
+    time = models.CharField(max_length=255, verbose_name="时间")
+    cost = models.IntegerField(default=0, verbose_name="消费金额")
+    way = models.ForeignKey("RechargePay", on_delete=models.DO_NOTHING, verbose_name="账单支方式", default="", null=True,
+                            blank=True, db_constraint=False, )
+
+    class Meta:
+        verbose_name = 'Bill'
+        verbose_name_plural = verbose_name
+
+
+class Address(models.Model):
+    userid = models.CharField(max_length=255, verbose_name="用户id")
+    name = models.CharField(max_length=255, verbose_name="用户名")
+    phone = models.CharField(max_length=255, verbose_name="手机号")
+    area = models.CharField(max_length=255, verbose_name="地区")
+    address = models.CharField(max_length=255, verbose_name="地址")
+    postcode = models.CharField(max_length=255, verbose_name="邮编")
+    isDefault = models.BooleanField(verbose_name="是否默认", default=False)
+
+    class Meta:
+        verbose_name = '地址信息'
+        verbose_name_plural = verbose_name
