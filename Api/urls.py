755b2390681bb7dfa10abe336ce8f43f7903191f
zgl <zhouguanglu2020@163.com>
2021-01-21 08:08:52 +0800

    添加了多urls分配

---
+++ Api/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2021/1/21 8:02
+@Author : Administrator
+@Email : zhouguanglu2012@163.com
+@File : urls.py
+@Project : SchoolOnline
+@Description：
+"""
+from django.urls import path,re_path,include
+from Api import  views
+
+urlpatterns = [
+    path('', views.zhwdsb,name='dsb'),
+    re_path(r'^index.html$',views.zhwdsb2,name='dsb2')
+]
--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def zhwdsb(request):
+    return HttpResponse("真是SB")
+
+def zhwdsb2(request):
+    return HttpResponse("真是2SB")
